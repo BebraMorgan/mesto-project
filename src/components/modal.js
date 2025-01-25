1405352c1b041a88177cfd2c0579456ebde2f5fa
ivans <shoiv@tavrida.com>
2025-01-25 05:09:35 +0300

    feat: добавить закрытие модальных окон на ESC и при клике на оверлей

--- src/components/modal.js
+++ src/components/modal.js
@@ -7,5 +7,12 @@ const closeModal = (popup) => {
     popup.classList.remove('popup_is-opened');
 }
 
+function closeByEsc(evt) {
+    if (evt.key === "Escape") {const openedPopup = document.querySelector('.popup_is-opened');
+        closeModal(openedPopup);
+    }
+}
 
-export default {openModal, closeModal};
+
+
+export default {openModal, closeModal, closeByEsc};
